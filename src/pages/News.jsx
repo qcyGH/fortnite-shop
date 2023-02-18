b1545e4844c80681630c578367797a957eeeb59e
Max <12maks32@gmail.com>
2023-02-18 21:53:22 +0100

    Refactored News page for use SSR

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { NewsList } from '@/components/NewsList'
 import { PreloaderNews } from '@/components/Preloader'
-import { useGetNewsQuery } from '@/store/fortniteApi'
 
-export default function News() {
+export default function News(props) {
 
-  const { data, isLoading } = useGetNewsQuery()
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const data = props.data
+
+    if (data) {
+      setLoading(false)
+    }
+  }, [props])
+
 
   return (
     <>
@@ -17,15 +25,15 @@ export default function News() {
       {
         !isLoading ? <>
           {
-            (data?.data.br?.image || data?.data.br?.messages) && <h2 className='mt-10 text-center text-2xl font-semibold text-gray-700 dark:text-gray-200 transition-color duration-150 ease-in'>
+            (props.data?.data.br?.image || props.data?.data.br?.messages) && <h2 className='mt-10 text-center text-2xl font-semibold text-gray-700 dark:text-gray-200 transition-color duration-150 ease-in'>
               Battle Royale
             </h2>
           }
           {
-            data?.data.br?.image && <img className='mt-5 rounded-lg shadow-lg shadow-zinc-400/50 dark:shadow-zinc-900/50 w-max h-max hover:shadow-none hover:scale-95 transition-all duration-150 ease-in' src={data?.data.br.image} alt='fortnite battle pass'/>
+            props.data?.data.br?.image && <img className='mt-5 rounded-lg shadow-lg shadow-zinc-400/50 dark:shadow-zinc-900/50 w-max h-max hover:shadow-none hover:scale-95 transition-all duration-150 ease-in' src={props.data?.data.br.image} alt='fortnite battle pass'/>
           }
           {
-            data?.data.br?.messages &&<NewsList items={data?.data.br.messages} />
+            props.data?.data.br?.messages &&<NewsList items={props.data?.data.br.messages} />
           }
         </> : <PreloaderNews isLoading={isLoading} />
       }
@@ -33,18 +41,31 @@ export default function News() {
       {
         !isLoading ? <>
             {
-              (data?.data.stw?.image || data?.data.stw?.messages) && <h2 className='mt-10 text-center text-2xl font-semibold text-gray-700 dark:text-gray-200 transition-color duration-150 ease-in'>
+              (props.data?.data.stw?.image || props.data?.data.stw?.messages) && <h2 className='mt-10 text-center text-2xl font-semibold text-gray-700 dark:text-gray-200 transition-color duration-150 ease-in'>
                 Save the World
               </h2>
             }
             {
-              data?.data.stw?.image && <img className='mt-5 rounded-md' src={data?.data.stw.image} alt='fortnite save the world'/>
+              props.data?.data.stw?.image && <img className='mt-5 rounded-md' src={props.data?.data.stw.image} alt='fortnite save the world'/>
             }
             {
-              data?.data.stw?.messages &&<NewsList items={data?.data.stw.messages} />
+              props.data?.data.stw?.messages &&<NewsList items={props.data?.data.stw.messages} />
             }
         </> : <PreloaderNews isLoading={isLoading} />
       }
     </>
   )
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://fortnite-api.com/v2/news`, {
+    headers: {
+      Authorization: process.env.API_KEY,
+    }
+  })
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
 }
