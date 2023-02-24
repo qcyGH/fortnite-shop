e0dbfb18bb9ae8a9975a50eacd13c946afcae388
Max <12maks32@gmail.com>
2023-02-24 16:54:38 +0100

    Change font to Manrope
    Now usgin Font optimization by Next

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
+import { Manrope } from '@next/font/google'
 import Layout from '@/components/layout'
 import Head from 'next/head'
+
+const manrope = Manrope({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
   return (
@@ -8,6 +11,11 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Fortnite shop</title>
       </Head>
+      <style jsx global>{`
+        body, html {
+          font-family: ${manrope.style.fontFamily} !important;
+        }
+      `}</style>
       <Component {...pageProps} />
     </Layout>
   )
